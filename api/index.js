d35c026c36be111e88a6106937a80ce4562e2522
taalhaataahir0102 <23100293@lums.edu.pk>
2025-08-29 14:41:49 +0500

    cors origin

--- api/index.js
+++ api/index.js
@@ -4,11 +4,20 @@ const dotenv = require("dotenv");
 const connectDB = require("../src/config/db");
 const counterRoutes = require("../src/routes/counterRoutes");
 
+
+const corsOptions = {
+    origin: 'https://frontend-taupe-rho-50.vercel.app/', // Replace with your frontend URL
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+    optionsSuccessStatus: 204 // Some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+
+
 dotenv.config();
 connectDB();
 
 const app = express();
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 
 // API routes
